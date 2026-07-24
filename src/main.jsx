a881b39a444193d6260921f91102fd8043e466c1
juliuspollak <99386240+juliuspollak@users.noreply.github.com>
2026-07-25 09:59:38 +1000

    Add Slovak localisation and compact account menu

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
+import { I18nProvider } from "./lib/i18n.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary onReset={() => window.location.reload()}>
-      <App />
-    </ErrorBoundary>
+    <I18nProvider>
+      <ErrorBoundary onReset={() => window.location.reload()}>
+        <App />
+      </ErrorBoundary>
+    </I18nProvider>
   </React.StrictMode>
 );
