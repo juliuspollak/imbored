2908b3624b4308186e880d9752b1c598264b1d3a
juliuspollak <99386240+juliuspollak@users.noreply.github.com>
2026-07-26 17:02:36 +1000

    Update Queens.jsx

--- src/games/Queens.jsx
+++ src/games/Queens.jsx
@@ -5,6 +5,7 @@ import { rateDifficulty } from "../lib/saveStats.js";
 import DifficultyRating, { DifficultyRatingBadge } from "../DifficultyRating.jsx";
 import { Crown, Eraser, CornerUpLeft, Sparkles, WandSparkles, Timer as TimerIcon, HelpCircle, Lock } from "lucide-react";
 import { useI18n } from "../lib/i18n.jsx";
+import DaySelector from "../DaySelector";
 
 /* ---------------- puzzle generation ---------------- */
 
