d9a10eb15bdd269f7eb30ed11101b93709cad919
juliuspollak <99386240+juliuspollak@users.noreply.github.com>
2026-08-30 21:04:14 +1000

    Hide WKWebView keyboard accessory bar

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,39 @@
 import UIKit
 import Capacitor
+import ObjectiveC.runtime
+
+private enum KeyboardAccessoryHider {
+    private static var installed = false
+
+    static func install() {
+        guard !installed else { return }
+
+        let selector = NSSelectorFromString("inputAccessoryView")
+        let classNames = [
+            ["UI", "Web", "Browser", "View"].joined(),
+            ["WK", "Content", "View"].joined()
+        ]
+
+        var replacedAtLeastOneImplementation = false
+
+        for className in classNames {
+            guard let viewClass: AnyClass = NSClassFromString(className),
+                  let method = class_getInstanceMethod(viewClass, selector) else {
+                continue
+            }
+
+            let nilAccessoryView: @convention(block) (AnyObject) -> UIView? = { _ in nil }
+            let replacement = imp_implementationWithBlock(nilAccessoryView)
+            method_setImplementation(method, replacement)
+            replacedAtLeastOneImplementation = true
+        }
+
+        // WKContentView is created by WebKit. If it was not loaded yet, leave
+        // installed=false so applicationDidBecomeActive can retry once the
+        // Capacitor bridge has created its WKWebView.
+        installed = replacedAtLeastOneImplementation
+    }
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,7 +41,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Capacitor renders HTML form controls in WKWebView. Unlike native chat
+        // apps, WKWebView adds a Previous/Next/Done accessory bar above the iOS
+        // keyboard. Hide that WebKit accessory so chat uses the normal keyboard
+        // height and our own composer remains the only control above it.
+        DispatchQueue.main.async {
+            KeyboardAccessoryHider.install()
+        }
         return true
     }
 
@@ -25,32 +65,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // Use this method to release shared resources, save user data, and invalidate timers.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Retry after the Capacitor bridge has created its WKWebView.
+        KeyboardAccessoryHider.install()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Called when the application is about to terminate. Save data if appropriate.
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
+        // Keep Capacitor App API URL-open handling intact.
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
+        // Keep Capacitor universal-link handling intact.
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
